46b12ef41a9d8bca760d2350cf7fc1db1079d7bc
Evan Munz <emunz14@gmail.com>
2017-10-30 14:38:06 -0500

    practiced some functions in c sharp

--- first_csharp/Program.cs
+++ first_csharp/Program.cs
@@ -6,7 +6,51 @@ namespace first_csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Print numbers 1-255
+            for(int i = 1; i <= 255; i++){
+                Console.WriteLine(i);
+            }
+
+            // Print 1-100, numbers divisible by 3 or 5, but not both
+            for(int i = 1; i <=100; i++){
+                if(i % 3 == 0 && i % 5 == 0){
+                    continue;
+                }
+                if(i % 3 == 0){
+                    Console.WriteLine(i);
+                }
+                if(i % 5 == 0){
+                    Console.WriteLine(i);
+                }
+            }
+
+            // Print Fizz and Buzz and FizzBuzz
+            for(int i = 1; i <=100; i++){
+                if(i % 3 == 0 && i % 5 == 0){
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if(i % 3 == 0){
+                    Console.WriteLine("Fizz");
+                }
+                else if(i % 5 == 0){
+                    Console.WriteLine("Buzz");
+                }
+            }
+
+            // Generates random number between 2-4 and prints a corresponding phrase based on random number
+            Random rand = new Random();
+            for(int val = 0; val <= 10; val++)
+            {
+                
+                int num = rand.Next(2,5);
+                if(num == 2){
+                    Console.WriteLine("Fizz");
+                } else if(num == 3){
+                    Console.WriteLine("Buzz");
+                } else if(num == 4){
+                    Console.WriteLine("FizzBuzz");
+                } 
+            }
         }
     }
 }

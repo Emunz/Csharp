6e2aafd216c54c5eb08b1c672cc3995155a5ee3e
Evan Munz <emunz14@gmail.com>
2017-10-30 16:39:34 -0500

    added boxing and unboxing practice

---
+++ boxing_unboxing/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace boxing_unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> BoxedData = new List<object>();
+            BoxedData.Add(7);
+            BoxedData.Add(28);
+            BoxedData.Add(-1);
+            BoxedData.Add(true);
+            BoxedData.Add("chair");
+
+            int sum = 0;
+            foreach(var thing in BoxedData){
+                Console.WriteLine(thing);
+                if(thing is int){
+                    sum += (int)thing;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
